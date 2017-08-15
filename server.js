a3c6e8a6d17977dac6190021c52a71228da74e11
Tuan Anh Ha <hatuananhvnhn@gmail.com>
2017-08-15 17:02:10 +0200

    add Projects page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'All projects created by Tuan Anh Ha'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Page not found!'
